e461e0c3f0617d932bc7a43f3738f2eb6c73abd0
ripeisripe <rip_e@hotmail.com>
2018-08-23 16:27:30 +0200

    Ici on a ajouté un service sur les crypto money

---
+++ forum/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
